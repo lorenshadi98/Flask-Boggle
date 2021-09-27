dadbafb92923f88130cc28e51740733076a3dbe5
lorenshadi98 <lorans.hadi@gmail.com>
2021-09-27 01:40:29 -0700

    Boggle game complete

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+
+from werkzeug.datastructures import ContentSecurityPolicy
+from werkzeug.wrappers.response import ResponseStream
 from boggle import Boggle
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, session, redirect
 from flask_cors import CORS
+
+GAME_BOARD = "board"
 
 app = Flask(__name__)
 CORS(app)
+
+app.config['SECRET_KEY'] = "never-tell!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 boggle_game = Boggle()
 board = boggle_game.make_board()
@@ -12,15 +20,27 @@ board = boggle_game.make_board()
 @app.route("/")
 def generate_board():
     """Generates board for boggle game"""
+    session[GAME_BOARD] = board
     return render_template("board.html", board=board)
 
 
 @app.route("/guess", methods=["POST", "GET"])
 def handle_data():
-    # TO DO: handle incoming server request and validate incoming "word"
-    # TO DO: check if the word exists in our dictionary, and then check if the word is valid in the game board.
-    # NOTE: make sure that you respond with JSON using the jsonify function from flask.
-    user_guess = request.args['guess']
-    word_exists = boggle_game.check_valid_word(board, user_guess)
+    """Handles data incoming from user guess form. Data handling includes 
+        validation as well as response"""
 
-    return jsonify({"result": word_exists, "word_length": len(user_guess)})
+    user_guess = request.args['guess']
+    board = session.get(GAME_BOARD)
+    word_exists = boggle_game.check_valid_word(board, user_guess)
+    return_json = jsonify(
+        {"result": word_exists, "word_length": len(user_guess)})
+    if request.method == "POST":
+        return return_json
+    else:
+        return return_json
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
--- static/app.js
+++ static/app.js
@@ -1,10 +1,17 @@
+let score = 0;
+let highscore = 0;
+let played_count = 0;
+let done_playing = false;
+// Handles guess form submission
 $('#word-form').on('submit', function (evt) {
   evt.preventDefault();
   user_guess = $('#guess').val().toLowerCase();
-  handleFormData(user_guess);
+  if (done_playing === false) {
+    handleFormData(user_guess);
+  }
   $('#guess').val('');
 });
-let score = 0;
+// Handles form data and creates axios request to Flask
 async function handleFormData(user_guess) {
   axios
     .post(
@@ -18,17 +25,29 @@ async function handleFormData(user_guess) {
     )
     .then(function (response) {
       //handle success
+      console.log(response);
       // update score and result text
       if (response.data.result === 'Correct!') {
         score = score + response.data.word_length;
       }
       $('#score').text('Score:' + score);
       $('#result').text(response.data.result);
-
-      console.log(response.data.result);
     })
     .catch(function (response) {
       //handle error
       console.log(response);
     });
+}
+// Handles timer for entire game board. 60 sec max time
+setTimeout(function () {
+  handle_game_end();
+}, 60000);
+
+// Handles game end
+function handle_game_end() {
+  done_playing = true;
+  $('#result').text('Times up! Refreshing page in 10 seconds');
+  setTimeout(function () {
+    location.reload();
+  }, 10000);
 }
